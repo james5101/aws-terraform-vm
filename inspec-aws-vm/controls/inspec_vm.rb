45be24e9c410dfc5411ca4e4bd5dcc48d1a81837
james5101 <james.c.noonan@gmail.com>
2019-06-01 21:59:30 -0400

    fi up test

--- inspec-aws-vm/controls/inspec_vm.rb
+++ inspec-aws-vm/controls/inspec_vm.rb
@@ -6,9 +6,5 @@ vm_name = "jamestest"
 
 describe aws_ec2_instance("i-00f15a5c34a317ba1") do
   it { should exist }
-end
-
-
-describe aws_ec2_instance(name: vm_name) do
-  it { should be_running }
+  it {should_be_running}
 end
